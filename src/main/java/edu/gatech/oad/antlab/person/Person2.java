2ba431485d1486e506a4b8057925ab6910ed4c54
Sai Naga Manoj Paladugu <paladugu.sainaga@gmail.com>
2014-01-21 18:29:55 -0500

    I have changed my implementation to be more efficient. - Sai

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,6 +1,7 @@
 package edu.gatech.oad.antlab.person;
 
 import java.util.Random;
+import java.util.LinkedList;
 
 /**
  *  A simple class for person 2
@@ -13,48 +14,51 @@ import java.util.Random;
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
-	/**
-	 * This method should take the string
-	 * input and return its characters in
-	 * random order.
-	 * given "gtg123b" it should return
-	 * something like "g3tb1g2".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  boolean completed = false;
-          String randomString = new String();
-          Random rand = new Random();
-	  
-	  while(randomString.length() != input.length()) {
-	  	int index = rand.nextInt(input.length());
-		if(!randomString.contains(input.charAt(index))) {
-			randomStirng += input.charAt(index);
- 		}
-	  }
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+    public Person2(String pname) {
+	name = pname;
+    }
+    /**
+     * This method should take the string
+     * input and return its characters in
+     * random order.
+     * given "gtg123b" it should return
+     * something like "g3tb1g2".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private static String calc(String input) {
+	//Person 2 put your implementation here
+	String randomString = new String();
+	Random rand = new Random();
+	LinkedList<Integer> list = new LinkedList<Integer>();
+	while(randomString.length() != input.length()) {
+	    int index = rand.nextInt(input.length());
+	    if(!list.contains(index)) {
+		randomString += input.charAt(index);
+		list.add(index);
+	    }
+	}
 
-	  return randomString;
-	}
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+	return randomString;
+    }
+
+
+
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the 
+     *         object
+     */
+    public String toString(String input) {
+	return name + calc(input);
+    }
 }
