72365c211cc8e9bb76aa85b4eb6ef04ea74fca22
Sai Naga Manoj Paladugu <paladugu.sainaga@gmail.com>
2014-01-20 20:18:37 -0500

    I just fixed up the indentation in the AntLabMain.java file so everyone retians their sanity. - Sai

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -54,31 +54,31 @@ import edu.gatech.oad.antlab.pkg2.*;
         
         String toPrint = 
             ant11.getMessage() + ant12.getMessage() + ant21.getMessage()
-          + ant22.getMessage() + ant31.getMessage();
-		  //Person1 replace P1 with your name
-		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("gburdell1");
-		  //Person2 replace P2 with your name
-		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("Sai Naga Manoj Paladugu");
-		  toPrint += p2.toString("spaladugu3");
-		  //Person3 replace P3 with your name
-		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
-          //Person4 replace P4 with your name
-          //and gburdell4 with your gt id
-          Person4 p4 = new Person4("Julia Ting");
-          toPrint += p4.toString("jting8");
-          //Person5 replace P4 with your name
-          //and gburdell5 with your gt id
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("gburdell5");
-          //Person6 replace P6 with your name
-          //and gburdell6 with your gt id
-          Person6 p6 = new Person6("P6");
-          toPrint += p6.toString("gburdell6");
+	    + ant22.getMessage() + ant31.getMessage();
+	//Person1 replace P1 with your name
+	//and gburdell1 with your gt id
+	Person1 p1 = new Person1("P1");
+	toPrint += p1.toString("gburdell1");
+	//Person2 replace P2 with your name
+	//and gburdell with your gt id
+	Person2 p2 = new Person2("Sai Naga Manoj Paladugu");
+	toPrint += p2.toString("spaladugu3");
+	//Person3 replace P3 with your name
+	//and gburdell3 with your gt id
+	Person3 p3 = new Person3("P3");
+	toPrint += p3.toString("gburdell3");
+	//Person4 replace P4 with your name
+	//and gburdell4 with your gt id
+	Person4 p4 = new Person4("Julia Ting");
+	toPrint += p4.toString("jting8");
+	//Person5 replace P4 with your name
+	//and gburdell5 with your gt id
+	Person5 p5 = new Person5("P5");
+	toPrint += p5.toString("gburdell5");
+	//Person6 replace P6 with your name
+	//and gburdell6 with your gt id
+	Person6 p6 = new Person6("P6");
+	toPrint += p6.toString("gburdell6");
 		  
         System.out.println(toPrint);
         
